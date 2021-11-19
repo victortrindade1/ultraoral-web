f0a24aac5440a83aa5873ad1a3955d8793ae5053
Victor Trindade <victortrindade@gmail.com>
2021-11-19 10:20:26 -0300

    page contact finished

--- src/pages/contato.tsx
+++ src/pages/contato.tsx
@@ -10,33 +10,22 @@ import {
   FaRegEnvelope,
   FaMapMarkerAlt,
   FaBuilding
-  // FaInstagram,
-  // FaFacebook
 } from 'react-icons/fa'
-import { useMediaQuery } from '@mui/material'
 
 import { companyInfo } from '../config/companyInfo'
 import theme from '../styles/theme'
 
-import {
-  Container,
-  AddressContainer,
-  Text
-  // SocialMediaContainer,
-  // DividerStyled
-} from '../styles/pages/Contato'
+import { Container, AddressContainer, Text } from '../styles/pages/Contato'
 
 const Contato: NextPage = () => {
   const title = 'Contato'
   const subtitle = 'Gente Odontologia'
 
-  const isBreakpoint = useMediaQuery('(max-width:768px)')
-
   return (
     <Layout title="Contato" description={`${title} - ${subtitle}`}>
       <Container>
         <PageTitle title={title} subtitle={subtitle} />
-        <AddressContainer isBreakpoint={isBreakpoint}>
+        <AddressContainer>
           <FaWhatsapp color={theme.palette.primary.main} />
           <Text>{companyInfo.whatsapp}</Text>
           <FaPhone color={theme.palette.primary.main} />
@@ -48,24 +37,6 @@ const Contato: NextPage = () => {
           <FaBuilding color={theme.palette.primary.main} />
           <Text>CNPJ: {companyInfo.cnpj}</Text>
         </AddressContainer>
-        {/* <DividerStyled />
-        <SocialMediaContainer>
-          <a
-            target="_blank"
-            href={companyInfo.instagram_url}
-            rel="noopener noreferrer"
-          >
-            <FaInstagram color={theme.palette.secondary.light} />
-          </a>
-          <a
-            target="_blank"
-            href={companyInfo.facebook_url}
-            rel="noopener noreferrer"
-          >
-            <FaFacebook color={theme.palette.secondary.light} />
-          </a>
-        </SocialMediaContainer>
-       */}
       </Container>
     </Layout>
   )
--- src/styles/pages/Contato.ts
+++ src/styles/pages/Contato.ts
@@ -1,37 +1,17 @@
 import styled from 'styled-components'
-import Divider from '@mui/material/Divider'
 
-interface IResponsive {
-  readonly isBreakpoint: boolean
-}
+export const Container = styled.div``
 
-export const Container = styled.div`
-  /* background-color: ${props => props.theme.palette.primary.dark};
-  width: 80vw;
-  max-height: 80vh;
-  margin: auto;
-  border-radius: 8px;
-  padding: 5vw;
-  font-family: 'Roboto', sans-serif;
-
-  display: flex;
-  flex-direction: column;
-  align-items: flex-end;
-  position: relative; */
-`
-
-export const AddressContainer = styled.div<IResponsive>`
+export const AddressContainer = styled.div`
   display: grid;
   grid-template-columns: 0.01fr 1fr;
-  grid-column-gap: ${props => (props.isBreakpoint ? '10px' : '28px')};
-  grid-row-gap: ${props => (props.isBreakpoint ? '30px' : '30px')};
+  grid-column-gap: 28px;
+  grid-row-gap: 30px;
   align-items: center;
   color: ${props => props.theme.palette.primary.contrastText};
-  font-size: ${props => (props.isBreakpoint ? '1rem' : '1.5rem')};
+  font-size: 1.5rem;
   font-family: 'Roboto', sans-serif;
-  padding: ${props =>
-    props.isBreakpoint ? '5vw 5vw 15vw 5vw' : '2vw 20vw 15vw 20vw'};
-
+  padding: 2vw 20vw 15vw 20vw;
   > svg {
     font-size: 1.5rem;
   }
@@ -39,24 +19,4 @@ export const AddressContainer = styled.div<IResponsive>`
 
 export const Text = styled.div`
   word-break: break-word;
-`
-
-export const SocialMediaContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  font-size: 2rem;
-  margin-bottom: 1em;
-
-  > a {
-    margin: 0vw 2vw;
-  }
-`
-
-export const DividerStyled = styled(Divider)`
-  display: block;
-  height: 1px;
-  border: 0;
-  border-top: 1px solid ${props => props.theme.palette.primary.contrastText};
-  margin: 3em 0;
-  padding: 0;
 `
