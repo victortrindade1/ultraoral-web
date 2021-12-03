bbadb8c1d0a18b806ef9801a36d1997c2d93da25
Victor Trindade <victortrindade@gmail.com>
2021-12-02 21:41:31 -0300

    logo

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -4,7 +4,7 @@ const theme = createTheme({
   palette: {
     primary: {
       light: '#757ce8',
-      main: '#d4af37',
+      main: '#be7e69',
       dark: '#333333',
       contrastText: '#f4f4f4'
     },
@@ -12,10 +12,11 @@ const theme = createTheme({
       light: '#999',
       main: '#f44336',
       dark: '#222222',
+      // dark: '#000000',
       contrastText: '#000'
     },
     warning: {
-      main: '#d4af37'
+      main: '#be7e69'
     },
     info: {
       main: '#f4f4f4'
